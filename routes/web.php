ae87a321e68e82d3fdeac0f0598746ab3702da95
Mzwandile Dladla <mzwandile.dladla@siyakha.co.za>
2021-02-26 04:35:46 +0200

    created crud function for the blog posts and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('welcome');
+});
+Route::get('/addpost', function () {
+    return view('welcome');
+});
+Route::get('//edit/:id', function () {
+    return view('welcome');
+});
+
+Route::resource('/addpost', [UserController::class]);
+
+
+
